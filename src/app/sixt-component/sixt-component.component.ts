3584c251601e87ff17da91f8f0658ce72943529a
Nilvan Lopes <nilvanlopes@outlook.com>
2026-01-05 23:43:25 -0300

    feat: update dependencies manager to pnpm, sixt component training list and ngFor

--- src/app/sixt-component/sixt-component.component.ts
+++ src/app/sixt-component/sixt-component.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { Phone } from '../types/phone';
 
 @Component({
   selector: 'app-sixt-component',
   templateUrl: './sixt-component.component.html',
-  styleUrls: ['./sixt-component.component.css']
+  styleUrls: ['./sixt-component.component.css'],
 })
-export class SixtComponentComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class SixtComponentComponent {
+  phones: Phone[] = [
+    { id: 1, name: 'Iphone 13', description: "don't buy this", soldOut: true },
+    { id: 2, name: 'Flip 7', description: 'too expensive', soldOut: false },
+    { id: 3, name: 'Fold 4', description: 'almost good', soldOut: true },
+  ];
 }
---
+++ src/app/types/phone.ts
@@ -0,0 +1,6 @@
+export interface Phone {
+  id: number;
+  name: string;
+  description: string;
+  soldOut: boolean;
+}
